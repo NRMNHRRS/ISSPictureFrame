fc225b6ee93662770f6e95b9d47420f27e0ce204
NRMNHRRS <nrmnhrrs@aol.com>
2017-03-26 22:49:48 -0400

    Update both-streams.sh
    The closing quotation should be a straight quotation mark. After changing the mark the script works great

--- both-streams.sh
+++ both-streams.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true
 do
-( cmdpid=$BASHPID; (sleep 300; kill $cmdpid) & exec livestreamer http://ustream.tv/channel/iss-hdev-payload mobile_240p --player omxplayer --fifo --player-args "--win \"0 0 720 480\" {filename}”)
+( cmdpid=$BASHPID; (sleep 300; kill $cmdpid) & exec livestreamer http://ustream.tv/channel/iss-hdev-payload mobile_240p --player omxplayer --fifo --player-args "--win \"0 0 720 480\" {filename}")
 sleep 5
 ( cmdpid=$BASHPID; (sleep 300; kill $cmdpid) & exec livestreamer http://ustream.tv/channel/live-iss-stream mobile_284p --player omxplayer --fifo --player-args "--win \"0 0 720 480\" {filename}")
 done
